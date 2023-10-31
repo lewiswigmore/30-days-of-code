e2d5c198394c6f98225e324df696d9968bb993c7
Lewis Wigmore <lewis.wigmore@protonmail.com>
2023-10-31 16:22:11 +0000

    Added day 12

--- Day 12: Inheritance.py
+++ Day 12: Inheritance.py
@@ -1,37 +1,47 @@
+# Define a class for a Person.
 class Person:
-	def __init__(self, firstName, lastName, idNumber):
-		self.firstName = firstName
-		self.lastName = lastName
-		self.idNumber = idNumber
-	def printPerson(self):
-		print("Name:", self.lastName + ",", self.firstName)
-		print("ID:", self.idNumber)
+    def __init__(self, firstName, lastName, idNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
 
+    def printPerson(self):
+        print("Name:", self.lastName + ",", self.firstName)
+        print("ID:", self.idNumber)
+
+# Define a class for a Student, which inherits from Person.
 class Student(Person):
     def __init__(self, firstName, lastName, idNumber, scores):
         super().__init__(firstName, lastName, idNumber)
         self.scores = scores
-    def calculate(self):
-        score = sum(scores)/len(scores)
-        if score >= 90:
-            return "O"
-        elif score >= 80:
-            return "E"
-        elif score >= 70:
-            return "A"
-        elif score >= 55:
-            return "P"
-        elif score >= 40:
-            return "D"
-        elif score < 40:
-            return "T"
 
+    def calculate(self):
+        # Calculate the average score.
+        score = sum(self.scores) / len(self.scores)
+        
+        # Determine the grade based on the score.
+        if score >= 90:
+            return "O"  # Outstanding
+        elif score >= 80:
+            return "E"  # Excellent
+        elif score >= 70:
+            return "A"  # Acceptable
+        elif score >= 55:
+            return "P"  # Poor
+        elif score >= 40:
+            return "D"  # Dismal
+        else:
+            return "T"  # Troll
+
+# Read input for the student.
 line = input().split()
 firstName = line[0]
 lastName = line[1]
 idNum = line[2]
-numScores = int(input()) # not needed for Python
-scores = list( map(int, input().split()) )
+numScores = int(input())  # The number of scores (not needed for Python)
+scores = list(map(int, input().split()))
+
+# Create a Student object and calculate the grade.
 s = Student(firstName, lastName, idNum, scores)
 s.printPerson()
 print("Grade:", s.calculate())
