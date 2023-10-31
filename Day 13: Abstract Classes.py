447da3d1519df83fe423af4394905bbaece4aaa2
Lewis Wigmore <lewis.wigmore@protonmail.com>
2023-10-31 16:22:40 +0000

    Added day 13

--- Day 13: Abstract Classes.py
+++ Day 13: Abstract Classes.py
@@ -1,13 +1,11 @@
-
-    
-#Write MyBook class
+# Write MyBook class, which inherits from Book
 class MyBook(Book):
     def __init__(self, title, author, price):
-        # Use super to call constructor in parent class
+        # Use super to call the constructor in the parent class
         super().__init__(title, author)
         self.price = price
         
     def display(self):
         print(f'Title: {self.title}')
         print(f'Author: {self.author}')
-        print(f'Price: {price}')
+        print(f'Price: {self.price}')
