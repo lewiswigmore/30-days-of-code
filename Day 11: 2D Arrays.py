0059b6b6c6c7ef5d3ca6af46f7d9a1413101cfa7
Lewis Wigmore <lewis.wigmore@protonmail.com>
2023-10-31 16:21:20 +0000

    Added day 11

--- Day 11: 2D Arrays.py
+++ Day 11: 2D Arrays.py
@@ -1,27 +1,22 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
-
 if __name__ == '__main__':
-
     arr = []
 
+    # Read a 6x6 matrix from user input and store it in 'arr'.
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
-    
-    max = -float('inf')
+
+    max_sum = -float('inf')
     for i in range(1, 5):
         for j in range(1, 5):
-            sum = 0
-            sum += arr[i][j]
-            sum += arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1]
-            sum += arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1]
-            if sum > max:
-                max = sum
-    print(max)
+            # Calculate the hourglass sum.
+            hourglass_sum = 0
+            hourglass_sum += arr[i][j]
+            hourglass_sum += arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1]
+            hourglass_sum += arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1]
+
+            # Check if the current hourglass sum is greater than the maximum.
+            if hourglass_sum > max_sum:
+                max_sum = hourglass_sum
+
+    # Print the maximum hourglass sum.
+    print(max_sum)
