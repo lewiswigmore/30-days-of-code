bcb35985fe6cfcc2ac77e305a96ba50be3676409
Lewis Wigmore <lewis.wigmore@protonmail.com>
2023-10-31 16:17:05 +0000

    Added day 6

--- Day 6: Let's Review.py
+++ Day 6: Let's Review.py
@@ -1,6 +1,10 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Read an integer input from STDIN.
 x = int(input())
 
+# Loop through a range of 0 to x (exclusive).
 for i in range(0, x):
+    # Read a string input from STDIN.
     s = input()
+    
+    # Print the modified string, combining characters at even and odd indices with a space.
     print(str(s[0::2] + " " + s[1::2]))
