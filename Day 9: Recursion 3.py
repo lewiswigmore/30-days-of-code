07b0a11de049e310ff817d16a1b1d56fd11f05d4
Lewis Wigmore <lewis.wigmore@protonmail.com>
2023-10-31 16:19:40 +0000

    Added day 9

--- Day 9: Recursion 3.py
+++ Day 9: Recursion 3.py
@@ -1,24 +1,22 @@
-#!/bin/python3
-
-import math
 import os
-import random
-import re
-import sys
 
-# Interation
+# Define a function to compute the factorial using iteration.
 def iterativeFactorial(n):
+    # Initialize the factorial result to 1.
     f = 1
-    for i in range(1, n+1):
+    # Use a for loop to calculate the factorial.
+    for i in range(1, n + 1):
         f = f * i
     return f
-    
-# Recursion
+
+# Define a function to compute the factorial using recursion.
 def factorial(n):
+    # Base case: if n is less than 2, return 1.
     if n < 2:
         return 1
     else:
-        return n * factorial(n-1)
+        # Recursive case: return n multiplied by the factorial of (n-1).
+        return n * factorial(n - 1)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
