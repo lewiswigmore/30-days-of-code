a5ad3ac64af963a2dfbab240b756afe5f772147d
Lewis Wigmore <lewis.wigmore@protonmail.com>
2023-10-31 16:18:14 +0000

    Added day 8

--- Day 8: Dictionaries and Maps.py
+++ Day 8: Dictionaries and Maps.py
@@ -1,18 +1,25 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n = int(input())
+# Initialize an empty dictionary 'd' to store name and phone number pairs.
 d = {}
 
+# Read the number of entries from the user.
+n = int(input())
+
+# Populate the dictionary 'd' with name and phone number entries.
 for i in range(n):
-    x = input().split(" ")
-    d[x[0]] = x[1]
+    entry = input().split(" ")
+    d[entry[0]] = entry[1]
+
+# Continuously read names and look up their corresponding phone numbers.
 while True:
     try:
         name = input()
     except EOFError:
-        break
-    if name in d.keys():
+        break  # Exit the loop if there is no more input.
+
+    # Check if the 'name' exists in the dictionary 'd'.
+    if name in d:
+        # If found, print the name and its corresponding phone number.
         print(name, d.get(name), sep="=")
     else:
+        # If not found, print "Not found."
         print("Not found")
-        
-     
