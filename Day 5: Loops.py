dddc0222877185b067cf581437d05248ea6f8b20
Lewis Wigmore <lewis.wigmore@protonmail.com>
2023-10-31 16:16:37 +0000

    Added day 5

--- Day 5: Loops.py
+++ Day 5: Loops.py
@@ -1,6 +1,8 @@
-#!/bin/python3
-
 if __name__ == '__main__':
+    # Read an integer 'n' from user input
     n = int(input().strip())
+    
+    # Loop from 1 to 10 and print the multiplication table for 'n'
     for i in range(1, 11):
-        print(str(n) + " x " + str(i) + " = " + str(n*i))
+        # Print the equation and result for each multiplication
+        print(str(n) + " x " + str(i) + " = " + str(n * i))
