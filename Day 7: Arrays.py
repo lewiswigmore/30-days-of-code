49b4f0ce4f82ce2601b7bd0631869cadacea4437
Lewis Wigmore <lewis.wigmore@protonmail.com>
2023-10-31 16:17:48 +0000

    Added day 7

--- Day 7: Arrays.py
+++ Day 7: Arrays.py
@@ -1,8 +1,13 @@
-#!/bin/python3
-
 if __name__ == '__main__':
+    # Read an integer 'n' from the user's input.
     n = int(input().strip())
+    
+    # Read a list of integers 'arr' from user input and split it into individual integers.
     arr = list(map(int, input().rstrip().split()))
+    
+    # Reverse the list 'arr' and store it in 'r'.
     r = arr[::-1]
+    
+    # Iterate through the reversed list 'r' and print each element with a space at the end.
     for i in r:
         print(i, end=" ")
